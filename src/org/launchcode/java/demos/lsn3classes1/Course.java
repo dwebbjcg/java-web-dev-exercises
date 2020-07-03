705d1f9f70df6a540f0050e4e18da7cb06416e6b
dwebbjcg <63323184+dwebbjcg@users.noreply.github.com>
2020-07-03 11:07:41 -0400

    Classes pt1 exer commit

---
+++ src/org/launchcode/java/demos/lsn3classes1/Course.java
@@ -0,0 +1,40 @@
+package org.launchcode.java.demos.lsn3classes1;
+
+import java.util.ArrayList;
+
+class Course {
+
+    private String courseName;
+    private ArrayList numberOfStudents;
+    private int numberOfCredits;
+
+    public Course(String courseName, ArrayList numberOfStudents, int numberOfCredits) {
+        this.courseName = courseName;
+        this.numberOfStudents = numberOfStudents;
+        this.numberOfCredits = numberOfCredits;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public ArrayList getNumberOfStudents() {
+        return numberOfStudents;
+    }
+
+    protected void setNumberOfStudents(ArrayList numberOfStudents) {
+        this.numberOfStudents = numberOfStudents;
+    }
+
+    public int getNumberOfCredits() {
+        return numberOfCredits;
+    }
+
+    protected void setNumberOfCredits(int numberOfCredits) {
+        this.numberOfCredits = numberOfCredits;
+    }
+}
